ebd56828a428315f7d7ddc85dceb88bad0c54919
RobertoLayna <potter.laynar@gmail.com>
2026-07-29 16:46:35 -0500

    chore: update domain to roberto.nopalsoft.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://robertolayna.dev",
+  site: "https://roberto.nopalsoft.net",
   integrations: [sitemap()],
 });
